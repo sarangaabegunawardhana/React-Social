b1a653fc4b9983e5467356c6e7ed8465fe036dba
pramodsaranga <pramodsaranga31@gmail.com>
2023-02-15 22:52:21 +0530

    Updated context

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 import Topbar from '../../components/topbar/Topbar'
 
 export default function Home() {
   return (
+    <>
     <Topbar/>
+    <Sidebar/>
+    </>
   )
 }
