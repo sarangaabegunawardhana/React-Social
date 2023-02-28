301c4df774586afe8ffa3787ec4071f4e546341a
pramodsaranga <pramodsaranga31@gmail.com>
2023-02-28 08:03:03 +0530

    Added login

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,28 @@
+import "./register.css"
+
+export default function Register() {
+  return (
+    <div className="register">
+        <div className="registerWrapper">
+            <div className="registerLeft">
+                <h3 className="registerLogo">Lamasocial</h3>
+                <span className="registerDesc">
+                Connect with friends and the world around on you Lamasocial!
+                </span>
+            </div>
+            <div className="registerRight">
+                <div className="registerBox">
+                    <input placeholder="Username" className="registerInput" />
+                    <input placeholder="Email" className="registerInput" />
+                    <input placeholder="Password" className="registerInput" />
+                    <input placeholder="Password Again" className="registerInput" />
+                    <button className="registerButton">Log in</button>
+                    <button className="registerRegisterButton">
+                        Create a New Account
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
